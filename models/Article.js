e609018616153c83406af1c4bfd812652265afe6
Valentin Dumitrescu <val3ntin.dumitrescu@gmail.com>
2024-03-04 20:17:56 +0200

    Create Article.js schema

---
+++ models/Article.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const ArticleSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    default: 'published'
+  },
+  body: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      body: {
+        type: String,
+        required: true
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ]
+});
+
+module.exports = mongoose.model('articles', ArticleSchema);
